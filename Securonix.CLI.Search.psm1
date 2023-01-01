7dac8a08f3b4ab0b083a93d4f9ec973bc10fae6e
Brian <14955584+brian-reeder@users.noreply.github.com>
2023-01-01 12:54:03 -0600

    Create Search Module
    + Create Search Module
    + Add Module to Module Manifest
    + Add Generic Search API Function, Get-SecuronixSearchAPIResponse
    + Add Activity Search API Interface, Get-SecuronixActivityEvents

---
+++ Securonix.CLI.Search.psm1
@@ -0,0 +1,144 @@
+<#
+.DESCRIPTION
+Get-SecuronixSearchAPIResponse is a flexible controller used to process Securonix Search calls. Use the higher level interfaces to enforce parameter guidelines.
+
+.PARAMETER Url
+Url endpoint for your Securonix instance. Must be in the format https://<hostname or IPaddress>/Snypr
+
+.PARAMETER Token
+An API token to validate access. Use New-SecuronixApiToken to generate a new token.
+
+.INPUTS
+None. You cannot pipe objects to Get-SecuronixIncidentAPIResponse
+
+.OUTPUTS
+System.String. Get-SecuronixIncidentAPIResponse returns the API response. The API will respond with a JSON object for valid requests.
+
+.EXAMPLE
+
+.LINK
+#>
+function Get-SecuronixSearchAPIResponse {
+    [CmdletBinding(
+        PositionalBinding,
+        SupportsShouldProcess
+    )]
+	param(
+		[Parameter(Mandatory)]
+		[string] $Url,
+		[Parameter(Mandatory)]
+		[string] $Token,
+        [Parameter(Mandatory)]
+        [string] $query,
+
+        [string] $eventtime_from,
+        [string] $eventtime_to,
+        [string] $tz,
+        [bool] $prettyJson,
+        [int] $max,
+        [string] $queryId
+ 	)
+
+	Begin {
+		if($Url.EndsWith('/')) {
+			$Url = $Url.Remove($Url.Length-1, 1)   
+		}
+
+		$Header = [ordered]@{
+			'token' = $Token
+		}
+
+		$o = $PSBoundParameters.Remove('Url');
+		$o = $PSBoundParameters.Remove('Token');
+		
+		$paramsList = @()
+		foreach($param in $PSBoundParameters.Keys) {
+			$paramsList += "$($param)=$($PSBoundParameters[$param])"
+		}
+		
+		$Uri = "$Url/ws/spotter/index/search?$($paramsList -join '&')"
+	}
+
+	Process {
+        if($PSCmdlet.ShouldProcess($Uri, 'REST Method')) {
+            $response = Invoke-RestMethod -Uri $Uri -Headers $Header -Method Get
+            return $response.result
+        }
+	}
+
+	End {}
+}
+
+<#
+.DESCRIPTION
+Get-SecuronixActivityEvents prepares API parameters and calls the Securonix Search API endpoint to receive matching Event logs.
+
+.PARAMETER Url
+Url endpoint for your Securonix instance. Must be in the format https://<hostname or IPaddress>/Snypr
+
+.PARAMETER Token
+An API token to validate access. Use New-SecuronixApiToken to generate a new token.
+
+.INPUTS
+None. You cannot pipe objects to Get-SecuronixIncidentAPIResponse
+
+.OUTPUTS
+System.String. Get-SecuronixIncidentAPIResponse returns the API response. The API will respond with a JSON object for valid requests.
+
+.EXAMPLE
+
+.LINK
+#>
+function Get-SecuronixActivityEvents {
+    [CmdletBinding(
+        PositionalBinding,
+        SupportsShouldProcess
+    )]
+	param(
+		[Parameter(Mandatory)]
+		[string] $Url,
+		[Parameter(Mandatory)]
+		[string] $Token,
+        [Parameter(Mandatory)]
+        [string] $Query,
+        [Parameter(Mandatory)]
+        [string] $TimeStart,
+        [Parameter(Mandatory)]
+        [string] $TimeEnd,
+
+        [string] $TimeZone,
+        [bool] $PrettyJson,
+        [int] $Max,
+        [string] $QueryId
+	)
+
+	Begin {
+		$paramsTable = @{
+            'Query' = 'query'
+			'TimeStart' = 'eventtime_from'
+			'TimeEnd' = 'eventtime_to'
+			'TimeZone' = 'tz'
+			'PrettyJson' = 'prettyJson'
+			'Max' = 'max'
+			'QueryId' = 'queryId'
+		}
+
+        if($PSBoundParameters.ContainsKey('WhatIf')) {
+            $PSBoundParameters.Remove('WhatIf') |  Out-Null
+        }
+
+        $PSBoundParameters['Query'] = @('index=activity',$Query) -join ' AND '
+		$params = [ordered]@{}
+		foreach($param in $PSBoundParameters.Keys) {
+			$key = if($paramsTable.Keys -Contains $param) { $paramsTable[$param] } else { $param }
+			$params[$key] = $PSBoundParameters[$param]
+		}
+	}
+
+	Process {
+        $r = Get-SecuronixSearchAPIResponse @Params
+        return $r
+	}
+
+	End {}
+}
