f7e5bfaee3176aea0756832184ac0ce16437de3f
Brian <14955584+brian-reeder@users.noreply.github.com>
2023-01-08 21:34:20 -0600

    Add params test for Incident Module

--- Test/scnx.test.params.psm1
+++ Test/scnx.test.params.psm1
@@ -1,14 +1,19 @@
 $ScnxParams = @{
     WhatIf = $true
-    Url = 'DunderMifflin.securonix.com/Snypr'
-    Token = '12345678-90AB-CDEF-1234-567890ABCDEF'
-    Username = 'JimHalpert'
-    Password = 'seEcUrEpAsSwOrD'
-    WhiteListName = 'test_whitelist'
-    TenantName = 'PA-Scranton'
+    ActionName = 'Claim'
     EmployeeId = 'jhalpert'
+    IncidentId = '100107'
+    Password = 'seEcUrEpAsSwOrD'
     ResourceName = 'dundermifflin_mst365_azuread'
     ResourceGroupId = '35'
+    TenantName = 'PA-Scranton'
+    TimeStart_ms = '1641040200'
+    TimeEnd_ms = '1641144600'
+    Token = '12345678-90AB-CDEF-1234-567890ABCDEF'
+    Url = 'DunderMifflin.securonix.com/Snypr'
+    Username = 'JimHalpert'
+    WhiteListName = 'test_whitelist'
+    WorkflowName = 'SOCTeamReview'
 }
 
 Export-ModuleMember -Variable ScnxParams
