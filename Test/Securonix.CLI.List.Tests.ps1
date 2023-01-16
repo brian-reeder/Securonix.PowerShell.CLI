66075b9cb172e879ab3ca9cd42cfd363edd6f7b0
Brian <14955584+brian-reeder@users.noreply.github.com>
2023-01-16 01:20:17 -0600

    Configure Pester tests for List module
    Fixes #53

---
+++ Test/Securonix.CLI.List.Tests.ps1
@@ -0,0 +1,81 @@
+# Invoke-Pester -Output Detailed .\Test\*.Tests.ps1
+BeforeAll {
+    Remove-Module Securonix.CLI -ErrorAction SilentlyContinue
+    Import-Module $PSScriptRoot\..\Securonix.CLI.psd1
+
+    $Url = 'https://dundermifflin.securonix.net/Snypr'
+
+    $ResourceGroups = [xml]@'
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <resourceGroups> <resourceGroup> <name>Bluecoat Proxy</name> <type>Bluecoat Proxy</type> </resourceGroup> <resourceGroup> <name>Ironport Data</name> <type>Cisco Ironport Email</type> </resourceGroup> <resourceGroup> <name>Windchill Data</name> <type>Windchill</type> </resourceGroup> </resourceGroups>
+'@
+    $PolicyList = [xml]@'
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <policies> <policy> <createdBy>admin</createdBy> <createdOn>2013-11-09T16:13:23-06:00</createdOn> <criticality>Low</criticality> <description></description> <hql> FROM AccessAccount AS accessaccount, Resources AS resources, AccessAccountUser AS accessaccountuser WHERE ((accessaccount.resourceid = resources.id AND accessaccountuser.id.accountid = accessaccount.id )) AND ((accessaccountuser.id.userid = '-1'))</hql> <id>1</id> <name>Accounts that dont have Users</name> </policy> <policy> <createdBy>DocTeam</createdBy> <createdOn>2013-11-09T16:31:09-06:00</createdOn> <criticality>Medium</criticality> <description></description> <hql> FROM Users AS users, AccessAccountUser AS accessaccountuser, AccessAccount AS accessaccount, Resources AS resources WHERE ((users.id = accessaccountuser.id.userid AND accessaccountuser.id.accountid = accessaccount.id AND accessaccount.resourceid = resources.id )) AND ((users.status = '0'))</hql> <id>2</id> <name>Accounts that belong to terminated user</name> </policy> </policies>
+'@
+    $PeerGroupList = [xml]@'
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?><peerGroups><peerGroup> <criticality>Low</criticality><name>Advertising</name></peerGroup><peerGroup> <criticality>Low</criticality><name>Branding</name></peerGroup></peerGroups> 
+'@
+}
+
+Describe 'Get-SecuronixResourcegroupList' {
+    Context "When token is valid" {
+        BeforeEach {
+            Mock Invoke-RestMethod -Verifiable `
+                -MockWith { return $ResourceGroups } `
+                -ModuleName Securonix.CLI.List
+        }
+        It 'Given required parameters, it returns all resource groups.' {
+            $response = Get-SecuronixResourcegroupList -Url 'DunderMifflin.securonix.com/Snypr' -Token '12345678-90AB-CDEF-1234-567890ABCDEF'
+            Should -InvokeVerifiable
+            $response.resourceGroups.resourceGroup | Should -not -BeNullOrEmpty
+        }
+        It 'Given positional parameters, it returns all resource groups.' {
+            $response = Get-SecuronixResourcegroupList 'DunderMifflin.securonix.com/Snypr' '12345678-90AB-CDEF-1234-567890ABCDEF'
+            Should -InvokeVerifiable
+            $response.resourceGroups.resourceGroup | Should -not -BeNullOrEmpty
+        }
+    }
+}
+
+Describe 'Get-SecuronixPolicyList' {
+    Context "When token is valid" {
+        BeforeEach {
+            Mock Invoke-RestMethod -Verifiable `
+                -MockWith { return $PolicyList } `
+                -ModuleName Securonix.CLI.List
+        }
+        It 'Given required parameters, it returns all configured policies.' {
+            $response = Get-SecuronixPolicyList -Url 'DunderMifflin.securonix.com/Snypr' -Token '12345678-90AB-CDEF-1234-567890ABCDEF'
+            Should -InvokeVerifiable
+            $response.policies.policy | Should -not -BeNullOrEmpty
+        }
+        It 'Given positional parameters, it returns all configured policies.' {
+            $response = Get-SecuronixPolicyList 'DunderMifflin.securonix.com/Snypr' '12345678-90AB-CDEF-1234-567890ABCDEF'
+            Should -InvokeVerifiable
+            $response.policies.policy | Should -not -BeNullOrEmpty
+        }
+    }
+}
+
+Describe 'Get-SecuronixPeerGroupsList' {
+    Context "When token is valid" {
+        BeforeEach {
+            Mock Invoke-RestMethod -Verifiable `
+                -MockWith { return $PeerGroupList } `
+                -ModuleName Securonix.CLI.List
+        }
+        It 'Given required parameters, it returns all user peer groups.' {
+            $response = Get-SecuronixPeerGroupsList -Url 'DunderMifflin.securonix.com/Snypr' -Token '12345678-90AB-CDEF-1234-567890ABCDEF'
+            Should -InvokeVerifiable
+            $response.peerGroups.peerGroup | Should -not -BeNullOrEmpty
+        }
+        It 'Given positional parameters, it returns all user peer groups.' {
+            $response = Get-SecuronixPeerGroupsList 'DunderMifflin.securonix.com/Snypr' '12345678-90AB-CDEF-1234-567890ABCDEF'
+            Should -InvokeVerifiable
+            $response.peerGroups.peerGroup | Should -not -BeNullOrEmpty
+        }
+    }
+}
+
+AfterAll {
+    Remove-Module Securonix.CLI
+}
