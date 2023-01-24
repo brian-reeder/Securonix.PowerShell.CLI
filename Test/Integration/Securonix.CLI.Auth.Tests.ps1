ac67179652362b47487840248686e4182e26c4fc
Brian <14955584+brian-reeder@users.noreply.github.com>
2023-01-24 13:17:17 -0600

    Add integration test for auth

---
+++ Test/Integration/Securonix.CLI.Auth.Tests.ps1
@@ -0,0 +1,133 @@
+# Invoke-Pester -Output Detailed .\Test\*.Tests.ps1
+BeforeAll {
+    Remove-Module Securonix.CLI -ErrorAction SilentlyContinue
+    Import-Module $PSScriptRoot\..\..\Securonix.CLI.psd1
+
+    $config = Import-PowerShellDataFile -Path "$PSScriptRoot\config.psd1"
+
+    $instance = $config.instance
+    $url      = $config.url
+    $username = $config.username
+    $password = $config.password
+}
+
+Describe 'New-SecuronixApiToken' {
+    Context "When credentials are valid" {
+        It 'Given only the required parameters, it returns a token.' {
+            $token = New-SecuronixApiToken -Url $url -Username $username -Password $password
+        }
+        It 'Given a validity, it returns a token.' {
+            $token = New-SecuronixApiToken -Url $url -Username $username -Password $password -Validity 7
+        }
+        It 'Given only required positional parameters, it returns a token.' {
+            $token = New-SecuronixApiToken $url $username $password
+        }
+        It 'Given all positional parameters, it returns a token.' {
+            $token = New-SecuronixApiToken $url $username $password 7
+        }
+        AfterEach {
+            $token | Should -Match '[\w]{8}(\-[\w]{4}){3}\-[\w]{12}'
+        }
+    }
+}
+
+Describe 'Connect-SecuronixApi' {
+    Context "When credentials are valid" {
+        It 'Given the url required parameters, it sets a connection.' {
+            $response = Connect-SecuronixApi -Url $url -Username $username `
+                -Password $password
+        }
+        It 'Given the url optional parameters, it sets a connection.' {
+            $response = Connect-SecuronixApi -Url $url -Username $username `
+                -Password $password -Validity 7
+        }
+        It 'Given the instance required parameters, it sets a connection.' {
+            $response = Connect-SecuronixApi -Instance $instance -Username $username `
+                -Password $password
+        }
+        It 'Given the instance optional parameters, it sets a connection.' {
+            $response = Connect-SecuronixApi -Instance $instance -Username $username `
+                -Password $password -Validity 7
+        }
+        It 'Given the positional parameters, it sets a connection.' {
+            $response = Connect-SecuronixApi $instance $username $password
+        }
+        AfterEach {            
+            $env:scnx_token | Should -Match '[\w]{8}(\-[\w]{4}){3}\-[\w]{12}'
+            $env:scnx_url   | Should -Be $url
+            $response       | Should -Be 'Connected'
+
+            $env:scnx_token = ''
+            $env:scnx_url   = ''
+        }
+    }
+}
+
+
+Describe 'Confirm-SecuronixApiToken' {
+    Context "When token is valid" {
+        BeforeAll {
+            $token = New-SecuronixApiToken -Url $url -Username $username -Password $password
+        }
+        It 'Given the required parameters, it returns "Valid".' {
+            $response = Confirm-SecuronixApiToken -Url $url -Token $token
+        }
+        It 'Given the required positional parameters, it returns "Valid".' {
+            $response = Confirm-SecuronixApiToken $url $token
+        }
+        AfterEach {
+            $response | Should -be 'Valid'
+        }
+    }
+    Context "When connection is set" {
+        BeforeAll {
+            Connect-SecuronixApi -Instance $instance -Username $username -Password $password
+        }
+        It 'Given the required parameters, it returns "Valid".' {
+            $response = Confirm-SecuronixApiToken
+        }
+        AfterEach {
+            $response | Should -be 'Valid'
+        }
+        AfterAll {
+            $env:scnx_url   = ''
+            $env:scnx_token = ''
+        }
+    }
+}
+
+Describe 'Update-SecuronixApiToken' {
+    Context "When token is valid" {
+        BeforeAll {
+            $token = New-SecuronixApiToken -Url $url -Username $username -Password $password
+        }
+        It 'Given the required parameters, it returns "Success".' {
+            $response = Update-SecuronixApiToken -Url $url -Token $token
+        }
+        It 'Given the required positional parameters, it returns "Success".' {
+            $response = Update-SecuronixApiToken $url $token
+        }
+        AfterEach {
+            $response | Should -be 'Success'
+        }
+    }
+    Context "When connection is set" {
+        BeforeAll {
+            Connect-SecuronixApi -Instance $instance -Username $username -Password $password
+        }
+        It 'Given the required parameters, it returns "Valid".' {
+            $response = Update-SecuronixApiToken
+        }
+        AfterEach {
+            $response | Should -be 'Success'
+        }
+        AfterAll {
+            $env:scnx_url   = ''
+            $env:scnx_token = ''
+        }
+    }
+}
+
+AfterAll {
+    Remove-Module Securonix.CLI
+}
---
+++ Test/Integration/config.psd1
@@ -0,0 +1,24 @@
+@{
+    url      = 'https://dundermifflin.securonix.net/Snypr'
+    instance = 'dundermifflin'
+    username = 'jhalpert'
+    password = 'sEcUrEpAsSwOrD'
+
+    incidentid = '100107'
+    status     = 'CLAIMED'
+
+    timestart  = @{
+        epoch = '1566249473000'
+        datetime = '08/19/19 21:17:53'
+    }
+
+    timeend = @{
+        epoch = '1566335873000'
+        datetime = '08/20/19 21:17:53'
+    }
+
+    timeend30d = @{
+        epoch = '1568945873000'
+        datetime = '09/19/19 21:17:53'
+    }
+}
